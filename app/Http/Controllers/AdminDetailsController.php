5050dedf9c38e23e73730c1ad40cb6e59d4ed6b2
ubakarasamyavanze <55589338+ubakarasamyavanze@users.noreply.github.com>
2022-07-18 21:14:09 +0530

    Update AdminDetailsController.php

--- app/Http/Controllers/AdminDetailsController.php
+++ app/Http/Controllers/AdminDetailsController.php
@@ -15,6 +15,7 @@ class AdminDetailsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->username = $_SESSION['UserName'];
     }
 
     /**
